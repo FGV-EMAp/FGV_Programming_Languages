9bc592dacd9ad8b25e226e9e123a81c3ed67e1b0
Flavio C. Coelho <fccoelho@gmail.com>
2015-01-29 14:19:14 -0200

    adicionei projeto kivy

---
+++ Kivytut/pictures/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/kivy
+'''
+Pictures demo
+=============
+
+This is a basic picture viewer, using the scatter widget.
+'''
+
+import kivy
+kivy.require('1.8')
+
+from glob import glob
+from random import randint
+from os.path import join, dirname
+from kivy.app import App
+from kivy.logger import Logger
+from kivy.uix.scatter import Scatter
+from kivy.properties import StringProperty
+
+__version__ = "1.0"
+
+class Picture(Scatter):
+    '''Picture is the class that will show the image with a white border and a
+    shadow. They are nothing here because almost everything is inside the
+    picture.kv. Check the rule named <Picture> inside the file, and you'll see
+    how the Picture() is really constructed and used.
+
+    The source property will be the filename to show.
+    '''
+
+    source = StringProperty(None)
+
+
+class PicturesApp(App):
+
+    def build(self):
+
+        # the root is created in pictures.kv
+        root = self.root
+
+        # get any files into images directory
+        curdir = dirname(__file__)
+        for filename in glob(join(curdir, 'images', '*')):
+            try:
+                # load the image
+                picture = Picture(source=filename, rotation=randint(-30,30))
+                # add to the main field
+                root.add_widget(picture)
+            except Exception as e:
+                Logger.exception('Pictures: Unable to load <%s>' % filename)
+
+    def on_pause(self):
+        return True
+
+
+if __name__ == '__main__':
+    PicturesApp().run()
+
--- README.md
+++ README.md
@@ -2,3 +2,8 @@ FGV-Intro-Python
 ================
 
 Código utilizado no Curso de Verão de Introdução à Programação com a Linguagem Python
+
+Primeira Parte
+--------------
+
+
