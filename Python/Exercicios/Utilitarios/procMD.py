bbd4ee0ca2340a3fd90a78aa1105e77fa77341e8
fccoelho <fccoelho@gmail.com>
2019-09-04 13:01:11 -0300

    Completamos a inserção do código no markdown

--- Python/Exercicios/Utilitarios/procMD.py
+++ Python/Exercicios/Utilitarios/procMD.py
@@ -4,11 +4,27 @@ import os
 from pygments.lexers import guess_lexer
 
 class ProcessaTexto:
+    """
+    Processador de markdown para inserção de código.
+    """
     def __init__(self, caminho):
+        self.max_linhas = 15
+        self.regex = "\{([/\w+]+.*\w+.py)\}"
         self.caminho = caminho
+        self.codigo = None
         self.caminho_base = os.path.split(self.caminho)[0]
-        self.linhas = self.le_texto()
+        self.linhas = []
+        self.marcações = []
         self.nlinhas = len(self.linhas)
+        self.__processa()
+
+    def __processa(self):
+        self.linhas = self.le_texto()
+        for l in self.linhas:
+            m = self.encontra_referencias(l)
+            if m:
+                _, ling = self.carrega_codigo(m[0])
+        self.insere_codigo(codigo=self.codigo,linguagem=ling)
 
     def le_texto(self):
         with open(self.caminho, 'r') as f:
@@ -16,23 +32,38 @@ class ProcessaTexto:
         return linhas
 
     def encontra_referencias(self, linha):
-        marcações = re.findall("\{([/\w+]+.*\w+.py)\}", linha)
+        """
+        Dada a linha returna marcações se existirem
+        :param linha:  linha a ser analisada
+        :return: lista com marcações.
+        """
+        marcações = re.findall(self.regex, linha)
+        if marcações:
+            self.marcações.append(self.linhas.index(linha))
         return marcações
 
     def carrega_codigo(self, caminho):
         with open(os.path.join(self.caminho_base, caminho), 'r') as f:
             texto = f.read()
+        self.codigo = texto
         linguagem = guess_lexer(texto).name
         return texto, linguagem
+
+    def insere_codigo(self, codigo, linguagem):
+        linhas = codigo.split('\n')
+        linhas = linhas[:self.max_linhas]
+        codigo = '\n'.join(linhas)
+
+        codigo = '```{}\n'.format(linguagem)+ codigo +'\n```'
+        for m in self.marcações:
+            self.linhas[m] = re.sub(self.regex, codigo, self.linhas[m])
+        with open(self.caminho, 'w') as f:
+            f.writelines(self.linhas)
+
 
 
 if __name__ == "__main__":
     P = ProcessaTexto('../Jogos/README.md')
     print(P.linhas)
-    for l in P.linhas:
-        m = P.encontra_referencias(l)
-        if m:
-            _, ling = P.carrega_codigo(m[0])
-            print(m)
-            print(ling)
+
 
