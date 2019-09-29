d1588157000884eed4821420644665b4bd79d3c3
Flávio Codeço Coelho <fccoelho@gmail.com>
2019-09-29 19:19:32 -0300

    Started porting SIR with demography but it requires a working multinomial generator

--- D/Exercicios/Epidemia/epidemiad/source/models.d
+++ D/Exercicios/Epidemia/epidemiad/source/models.d
@@ -1,8 +1,9 @@
 /**
 Continuous time  Markov model implementing an SIR epidemic Model
 Author: Flávio Codeço Coelho<fccoelho@gmail.com>
+License: MIT
 */
-
+/** Copyright: Flávio Codeço Coelho */
 module models;
 
 import std.stdio;
@@ -11,11 +12,12 @@ import std.file;
 import std.typecons : tuple, Tuple;
 import core.exception : RangeError;
 import mir.random;
-import mir.random.variable: uniformVar, exponentialVar;
+import mir.random.variable : uniformVar, exponentialVar, binomialVar;
 import mir.ndslice;
 import mir.ndslice.fuse;
 import pyd.pyd;
 
+///SIR model class.
 class SIR
 {
     uint[3] inits;
@@ -30,20 +32,31 @@ class SIR
         this.beta = beta;
         this.gam = gam;
     }
-
+    /**
+    Set the initial values for state
+    Params:
+        S = Initial number of susceptibles
+        I = Initial number of infectious
+        R = Initial number of Removed
+    */
     void initialize(uint S, uint I, uint R)
     {
         this.S ~= S;
         this.I ~= I;
         this.R ~= R;
     }
+    /**
+    Starts simulation.
 
-    auto run(double t0, double tf, uint seed=7687738)
+    Params:
+        t0 = initial time
+        tf = final time
+    */
+    Tuple!(double[], uint[], uint[], double[]) run(double t0, double tf, uint seed = 7687738)
     {
         this.ts ~= t0;
         auto rng = Random(seed);
         auto urv = uniformVar!double(0.0, 1.0);
-
         double[] dts = [0];
         double R, U, pinf;
         while ((this.ts[$ - 1] < tf) & (this.I[$ - 1] > 0))
@@ -79,6 +92,96 @@ class SIR
     }
 }
 
+/**
+SIR model with demography (births and deaths.
+*/
+class SIR_Dem : SIR
+{
+    private
+    {
+        double alpha;
+    }
+    this(uint N, double alpha, double beta, double gamma)
+    {
+        super(N, beta, gamma);
+        this.alpha = alpha;
+    }
+
+    override Tuple!(double[], uint[], uint[], double[]) run(double t0, double tf, uint seed = 76838)
+    {
+        this.ts ~= t0;
+        auto rng = Random(seed);
+        auto urv = uniformVar!double(0.0, 1.0);
+        double[] dts = [0];
+        double R, U, pinf, pbirth, pds, pdi;
+        while ((this.ts[$ - 1] < tf) & (this.I[$ - 1] > 0))
+        {
+            U = urv(rng);
+            R = this.alpha * this.N + this.beta * this.S[$ - 1] * this.I[$ - 1]
+                / this.N + this.gam * this.I[$ - 1] + this.alpha * this.S[$ - 1]
+                + this.alpha * this.I[$ - 1];
+            pbirth = this.alpha*this.N/R;  /// Probability of the next event being a birth (S -> S+1)
+            pinf = ((this.beta/this.N)*this.S[$-1]*this.I[$-1])/R; /// Probability of next event being an infection
+            prec = this.gam*this.I[$-1]/R;  /// Probability of the next event being a recovery (I -> I-1)
+            pds = this.alpha*this.S[$-1]/R; /// Probability of the next event being a death of an S (S -> S-1)
+            pdi = this.alpha*this.I[$-1]/R;
+            ev = multinomial(1, [pbirth, pinf, prec, pds, pdi]).nonzero()
+            auto erv = exponentialVar!double(1.0 / R);
+            double dt = erv(rng);
+        }
+    }
+}
+
+/** The multinomial distribution has the form
+                                      N!           n_1  n_2      n_K
+   prob(n_1, n_2, ... n_K) = -------------------- p_1  p_2  ... p_K
+                             (n_1! n_2! ... n_K!)
+   where n_1, n_2, ... n_K are nonnegative integers, sum_{k=1,K} n_k = N,
+   and p = (p_1, p_2, ..., p_K) is a probability distribution.
+   Random variates are generated using the conditional binomial method.
+   This scales well with N and does not require a setup step.
+   Ref:
+   C.S. David, The computer generation of multinomial random variates,
+   Comp. Stat. Data Anal. 16 (1993) 205-217
+*/
+void multinomialVar (const gsl_rng * r, const size_t K,
+                     const unsigned int N, const double p[], unsigned int n[])
+{
+  size_t k;
+  double norm = 0.0;
+  double sum_p = 0.0;
+
+  uint sum_n = 0;
+
+  /* p[k] may contain non-negative weights that do not sum to 1.0.
+   * Even a probability distribution will not exactly sum to 1.0
+   * due to rounding errors.
+   */
+
+  for (k = 0; k < K; k++)
+    {
+      norm += p[k];
+    }
+
+  for (k = 0; k < K; k++)
+    {
+      if (p[k] > 0.0)
+        {
+          n[k] = gsl_ran_binomial (r, p[k] / (norm - sum_p), N - sum_n);
+        }
+      else
+        {
+          n[k] = 0;
+        }
+
+      sum_p += p[k];
+      sum_n += n[k];
+    }
+
+}
+
+
+
 
 
 /**
@@ -87,11 +190,7 @@ class SIR
 extern (C) void PydMain()
 {
     module_init();
-
-    wrap_class!(
-        SIR,
-        Def!(SIR.initialize),
-        Def!(SIR.run),
-        Init!(uint, double, double))();
+    wrap_class!(SIR, Def!(SIR.initialize), Def!(SIR.run), Init!(uint, double, double))();
+    wrap_class!(SIR_Dem, Init!(uint, double, double, double))();
 
 }
