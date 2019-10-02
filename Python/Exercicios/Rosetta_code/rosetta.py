d0a75a6e595bd7095d8fb0502485f8eef5d75d05
Flávio Codeço Coelho <fccoelho@gmail.com>
2019-10-02 11:02:40 -0300

    Atualizei rosetta.py

--- Python/Exercicios/Rosetta_code/rosetta.py
+++ Python/Exercicios/Rosetta_code/rosetta.py
@@ -5,15 +5,15 @@ finds all tasks which are not implemented in that language.
 from functools import partial
 from operator import itemgetter
 import requests
- 
+
 URL = 'http://www.rosettacode.org/mw/api.php'
 REQUEST_PARAMETERS = dict(action='query',
                           list='categorymembers',
                           cmlimit=500,
                           rawcontinue=True,
                           format='json')
- 
- 
+
+
 def unimplemented_tasks(language, url, request_params):
     """Yields all unimplemented tasks for a specified language"""
     with requests.Session() as session:
@@ -26,8 +26,8 @@ def unimplemented_tasks(language, url, request_params):
         for task in all_tasks:
             if task not in language_tasks:
                 yield task
- 
- 
+
+
 def tasks_titles(session, url, **params):
     """Yields tasks names for a specified category"""
     while True:
@@ -38,11 +38,18 @@ def tasks_titles(session, url, **params):
         if query_continue is None:
             return
         params.update(query_continue['categorymembers'])
- 
- 
+
+
 if __name__ == '__main__':
-    tasks = unimplemented_tasks('Python',
-                                url=URL,
-                                request_params=REQUEST_PARAMETERS)
-    print(*tasks, sep='\n')
- 
+    tasksD = tuple(unimplemented_tasks('D',
+                                       url=URL,
+                                       request_params=REQUEST_PARAMETERS))
+    taskspy = tuple(unimplemented_tasks('Python',
+                                        url=URL,
+                                        request_params=REQUEST_PARAMETERS))
+    print("====\nProblemas sem soluçao em D:\n====\n")
+    print(*tasksD, sep='\n')
+    print("====\nProblemas sem soluçao em Python:\n====\n")
+    print(*taskspy, sep='\n')
+    print("====\nproblemas sem soluçao em D e nem Python:\n====\n")
+    print(*list(set(tasksD) & set(taskspy)), sep='\n')
