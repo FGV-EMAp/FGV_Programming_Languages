e589981edd541fec7bfb7e9344406a2d4c97d9c8
fccoelho <fccoelho@gmail.com>
2019-10-21 12:55:43 -0300

    Atualizei web app

--- Python/Exercicios/Aplicação Web/dataviz/graficos/models.py
+++ Python/Exercicios/Aplicação Web/dataviz/graficos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Grafico(models.Model):
+    tipo = models.CharField(max_length=16, choices=[('l','linha'),('b','barra')])
+    dadosx = models.CharField(max_length=1000,null=False)
+    dadosy = models.CharField(max_length=1000,null=False)
+
--- Python/Exercicios/Aplicação Web/dataviz/graficos/views.py
+++ Python/Exercicios/Aplicação Web/dataviz/graficos/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Grafico
 
 # Create your views here.
 
-
 def index(request):
-    return HttpResponse("alo mundo")
+    if request.method == 'GET':
+        return HttpResponse("alo mundo")
+    elif request.method == 'POST':
+        g = Grafico(tipo=tipo, datax=x, datay=y)
+        g.save()
+        cria_grafico(x,y,tipo)
+
+def cria_grafico(x,y,tipo):
+
+
+
