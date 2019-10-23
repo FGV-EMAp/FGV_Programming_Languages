daa8084eb628c0951c606dd59cce1f61b497c59c
Flávio Codeço Coelho <fccoelho@gmail.com>
2019-10-23 11:04:16 -0300

    Adicionei o formulario ao web app

--- Python/Exercicios/Aplicação Web/dataviz/graficos/views.py
+++ Python/Exercicios/Aplicação Web/dataviz/graficos/views.py
@@ -1,17 +1,45 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, render_to_response
+from django.views import generic
 from .models import Grafico
+from bokeh.plotting import figure, output_file, show
+from bokeh.embed import components
+
 
 # Create your views here.
 
 def index(request):
+    context = {'tipos': {'linha': 'l', 'Barra': 'l'},
+               'script': "",
+               'div': ""
+               }
     if request.method == 'GET':
-        return HttpResponse("alo mundo")
+        context['titulo'] = "Defina um grafico",
     elif request.method == 'POST':
+        tipo = request.POST.get('tipo', 'l')
+        x = request.POST.get('x', [])
+        y = request.POST.get('y', [])
         g = Grafico(tipo=tipo, datax=x, datay=y)
         g.save()
-        cria_grafico(x,y,tipo)
+        script, div = cria_grafico(x, y, tipo)
+        context.update({'titulo': "Seu grafico!",
+                        'script': script,
+                        'div': div
+                        })
+    return render_to_response('home.html', context)
 
-def cria_grafico(x,y,tipo):
+
+def cria_grafico(x, y, tipo):
+    if tipo == 'l':
+        plot = figure(title="Grafico de linha", x_axis_label='X', y_axis_label='Y', plot_width=800, plot_height=400)
+        plot.line(x, y, line_width=2)
+    else:
+        plot = figure(title="Grafico de linha", x_axis_label='X', y_axis_label='Y', plot_width=800, plot_height=400)
+        plot.vbar()
+
+    script, div = components(plot)
+    return script, div
 
 
-
+class GraficoView(generic.ListView):
+    template_name = 'grafico_list.html'
+    context_object_name = 'lista_de_graficos'
