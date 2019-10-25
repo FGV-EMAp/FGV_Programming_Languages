05da93d53db8924cbd8f0879c591570cf4610a5c
fccoelho <fccoelho@gmail.com>
2019-10-25 12:57:06 -0300

    Atualizei web app

--- Python/Exercicios/Aplicação Web/dataviz/graficos/models.py
+++ Python/Exercicios/Aplicação Web/dataviz/graficos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
 
@@ -7,3 +8,21 @@ class Grafico(models.Model):
     dadosx = models.CharField(max_length=1000,null=False)
     dadosy = models.CharField(max_length=1000,null=False)
 
+class FormularioDoGrafico(ModelForm):
+    class Meta:
+        model = Grafico
+        fields = ['tipo', 'dadosx', 'dadosy']
+        labels = {
+            'tipo': 'Tipo',
+            'dadosx': "valores de x",
+            'dadosy': "valores de y"
+        }
+        # help_texts = {
+        #     'tipo': 'Escolha o tipo do gráfico',
+        #     'dadosx': 'Entre com os valores de x'
+        # }
+        # error_messages = {
+        #     'tipo': {
+        #         'max_length': _("This writer's name is too long."),
+        #     },
+        # }
--- Python/Exercicios/Aplicação Web/dataviz/graficos/views.py
+++ Python/Exercicios/Aplicação Web/dataviz/graficos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse, render_to_response
 from django.views import generic
-from .models import Grafico
+from .models import Grafico, FormularioDoGrafico
 from bokeh.plotting import figure, output_file, show
 from bokeh.embed import components
 
@@ -14,23 +14,31 @@ def index(request):
                }
     if request.method == 'GET':
         context['titulo'] = "Defina um grafico"
+        context['form'] = FormularioDoGrafico()
     elif request.method == 'POST':
-        tipo = request.POST.get('Tipo', 'l')
-        x = request.POST.get('x', [])
-        y = request.POST.get('y', [])
-        g = Grafico(tipo=tipo, dadosx=x, dadosy=y)
-        g.save()
-        script, div = cria_grafico(x, y, tipo)
-        context.update({'titulo': "Seu grafico!",
-                        'script': script,
-                        'div': div
-                        })
+        form = FormularioDoGrafico(request.POST)
+        context['form'] = form
+        if form.is_valid():
+            tipo = request.POST.get('tipo', 'l')
+            x = request.POST.get('dadosx', [])
+            y = request.POST.get('dadosy', [])
+            g = Grafico(tipo=tipo, dadosx=x, dadosy=y)
+            g.save()
+            script, div = cria_grafico(x, y, tipo)
+            context.update({'titulo': "Seu grafico!",
+                            'script': script,
+                            'div': div
+                            })
+        else:
+            print(form.is_valid())
+            pass
+
     return render(request, 'home.html', context)
 
 
 def cria_grafico(x, y, tipo):
-    x = [float(i) for i in x.split(',')]
-    y = [float(i) for i in y.split(',')]
+    x = [float(i) for i in x.strip(',').split(',')]
+    y = [float(i) for i in y.strip(',').split(',')]
     if tipo == 'l':
         plot = figure(title="Grafico de linha", x_axis_label='X', y_axis_label='Y', plot_width=800, plot_height=400)
         plot.line(x, y, line_width=2)
